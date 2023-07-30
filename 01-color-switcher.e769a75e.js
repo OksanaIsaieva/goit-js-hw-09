d5fa417e7a133052d44a8ddbebd12861562c7daf
OksanaIsaieva <103386079+OksanaIsaieva@users.noreply.github.com>
2023-07-30 22:38:39 +0000

    Deploying to gh-pages from @ OksanaIsaieva/goit-js-hw-09@86b87c407313ce7cdeccc637214f638fc6e78ad3 🚀

---
+++ 01-color-switcher.e769a75e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;t.addEventListener("click",(function(t){this.disabled=!0,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e769a75e.js.map
