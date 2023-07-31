fff6a4fe35bc79cbb4038c9274649797a6e91de4
OksanaIsaieva <oksanka.isayeva@gmail.com>
2023-07-31 20:45:16 +0300

    hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from "notiflix";
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const form = document.querySelector('form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      if (shouldResolve) {
+        resolve({position: position, delay: delay});
+      } else {
+        reject({position: position, delay: delay});
+      }
+
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  
+  window.setTimeout(()=>{
+    for (let n = 0; n < parseInt(amount.value); n++) {
+
+      createPromise(n, parseInt(delay.value) + parseInt(step.value) * n)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  }, parseInt(delay.value));
+});
